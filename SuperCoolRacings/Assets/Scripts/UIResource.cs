eff2f55d542fc5fbfdb8b1dd555566e34036339f
Unknown <n.hammes@student.fontys.nl>
2018-04-20 20:36:19 +0200

    Created GameController, Resources, HouseFactory

---
+++ SuperCoolRacings/Assets/Scripts/UIResource.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Resource))]
+public class UIResource : MonoBehaviour {
+    
+    public Text Value;
+
+    private Resource resource;
+
+    public void Start()
+    {
+        resource = GetComponent<Resource>();
+        UpdateUI();
+        resource.OnValueChanged.AddListener(UpdateUI);
+    }
+
+
+    public void UpdateUI()
+    {
+        Value.text = resource.Amount.ToString();
+    }
+}
