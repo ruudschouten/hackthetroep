cdcd059b7704fe3c8e8078b435e0d8cbabde101f
Ruud Schouten <ruud.schouten@outlook.com>
2018-04-21 05:16:11 +0200

    Added Title Screen with working buttons

---
+++ SuperCoolRacings/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+    public void StartPlaying() {
+        SceneManager.LoadScene("GameScene_Island");
+    }
+
+    public void Exit() {
+    #if UNITY_EDITOR
+            // Application.Quit() does not work in the editor so
+            // UnityEditor.EditorApplication.isPlaying need to be set to false to end the game
+            UnityEditor.EditorApplication.isPlaying = false;
+    #else
+         Application.Quit();
+     #endif
+    }
+}
---
+++ SuperCoolRacings/Assets/Scripts/Showcase.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Showcase : MonoBehaviour {
+    public Vector3 this_Axis;
+    public float deg;
+    public float time;
+
+    private bool isRotating;
+    // Update is called once per frame
+    void Update() {
+        if (!isRotating) {
+            StartCoroutine(RotateAround(this_Axis, deg, time));
+        }
+    }
+    
+    IEnumerator RotateAround(Vector3 axis, float degrees, float duration){
+        if(axis == Vector3.zero || degrees == 0){
+            yield return true;
+        }
+        isRotating = true;
+        float d = 0;
+        Quaternion q;
+        Quaternion startRot = transform.rotation;
+        float progress = 0;    
+        while(progress <= 1){
+            d = Mathf.Lerp(0, degrees, progress);
+            q = Quaternion.AngleAxis(d, axis);
+            transform.rotation = startRot*q;
+            progress += Time.deltaTime/duration;
+            yield return null;
+        }
+        transform.rotation = startRot*Quaternion.AngleAxis(degrees, axis);
+        isRotating = false;
+    }
+}
