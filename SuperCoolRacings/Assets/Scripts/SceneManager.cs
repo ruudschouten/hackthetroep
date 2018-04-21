b7daad98acbac072d7f403e771e3426ec3b942db
Ruud Schouten <ruud.schouten@outlook.com>
2018-04-21 08:13:30 +0200

    Fixed scene loading

---
+++ SuperCoolRacings/Assets/Scripts/SceneManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneManager : MonoBehaviour {
+    public void StartPlaying() {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("GameScene_Island");
+    }
+
+    public void QuitGame() {
+        // save any game data here
+        #if UNITY_EDITOR
+                // Application.Quit() does not work in the editor so
+                // UnityEditor.EditorApplication.isPlaying need to be set to false to end the game
+                UnityEditor.EditorApplication.isPlaying = false;
+        #else
+         Application.Quit();
+     #endif
+    }
+}
